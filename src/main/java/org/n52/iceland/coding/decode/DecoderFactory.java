520d547c0644b558de4314737d0864d209ea15b2
Christian Autermann <christian@autermann.org>
2015-06-18 15:20:34 +0200

    fixed package name

--- src/main/java/org/n52/iceland/coding/decode/DecoderFactory.java
+++ src/main/java/org/n52/iceland/coding/decode/DecoderFactory.java
@@ -14,10 +14,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.n52.iceland.decode;
+package org.n52.iceland.coding.decode;
 
-import org.n52.iceland.coding.decode.Decoder;
-import org.n52.iceland.coding.decode.DecoderKey;
 import org.n52.iceland.component.ComponentFactory;
 
 /**
