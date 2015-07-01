5c284a01a608cf0d8bf0d82ccf9a8f7cc444d954
EHJ-52n <e.h.juerrens@52north.org>
2015-07-01 16:18:17 +0200

    Add deprecated fields and update version

--- src/main/java/org/n52/iceland/util/http/MediaTypes.java
+++ src/main/java/org/n52/iceland/util/http/MediaTypes.java
@@ -24,7 +24,7 @@ import com.google.common.collect.ImmutableSetMultimap;
  * @author Christian Autermann <c.autermann@52north.org>
  * @author <a href="mailto:e.h.juerrens@52north.org">Eike Hinderk J&uuml;rrens</a>
  * 
- * @since 4.0.0
+ * @since 1.0.0
  */
 public interface MediaTypes {
     
@@ -55,8 +55,14 @@ public interface MediaTypes {
     MediaType APPLICATION_NETCDF = new MediaType(APPLICATION, "netcdf");
     
     MediaType APPLICATION_GML_32 = new MediaType(APPLICATION, "gml+xml", "version", "3.2");
+
+	@Deprecated
+	MediaType APPLICTION_GML_32 = APPLICATION_GML_32;
     
     MediaType APPLICATION_OM_20 = new MediaType(APPLICATION, "om+xml", "version", "2.0");
+
+	@Deprecated
+	MediaType APPLICTION_OM_20 O APLLICATION_OM_20;
     
     ImmutableSetMultimap<MediaType, MediaType> COMPATIBLE_TYPES = new ImmutableSetMultimap
             .Builder<MediaType, MediaType>()
