d31216b55e440983eb34c50fee5e9fa64be31ce5
CarstenHollmann <c.hollmann@52north.org>
2015-08-27 14:06:46 +0200

    Refactor initialization of class variable and formatting

--- src/main/java/org/n52/iceland/ogc/ows/OwsParameterValuePossibleValues.java
+++ src/main/java/org/n52/iceland/ogc/ows/OwsParameterValuePossibleValues.java
@@ -23,16 +23,18 @@ import java.util.TreeSet;
 
 import org.n52.iceland.util.CollectionHelper;
 
+import com.google.common.collect.Sets;
+
 /**
  * @since 1.0.0
  * 
  */
 public class OwsParameterValuePossibleValues implements OwsParameterValue {
 
-    private SortedSet<String> values;
+    private SortedSet<String> values = Sets.newTreeSet();
 
     public OwsParameterValuePossibleValues(Collection<String> values) {
-        this.values = values == null ? new TreeSet<String>() : new TreeSet<String>(values);
+        setValues(values);
     }
 
     public OwsParameterValuePossibleValues(String value) {
@@ -42,30 +44,31 @@ public class OwsParameterValuePossibleValues implements OwsParameterValue {
     public OwsParameterValuePossibleValues(Enum<?> value) {
         this(value.name());
     }
-    
+
     public OwsParameterValuePossibleValues() {
+        this(new TreeSet<String>());
     }
 
     public SortedSet<String> getValues() {
         return Collections.unmodifiableSortedSet(values);
     }
-    
+
     public void addValue(String value) {
-    	addValues(Collections.singleton(value));
+        addValues(Collections.singleton(value));
     }
-    
+
     public void addValue(Enum<?> value) {
-    	addValues(Collections.singleton(value.name()));
+        addValues(Collections.singleton(value.name()));
     }
-    
+
     public void addValues(Collection<String> values) {
-    	if (isSetValues()) {
-    		if (values != null) {
+        if (isSetValues()) {
+            if (values != null) {
                 this.values.addAll(values);
             }
-    	} else { 
-    		this.values = values == null ? new TreeSet<String>() : new TreeSet<String>(values);
-    	}
+        } else {
+            this.values = values == null ? new TreeSet<String>() : new TreeSet<String>(values);
+        }
     }
 
     public void setValues(Collection<String> values) {
@@ -74,9 +77,9 @@ public class OwsParameterValuePossibleValues implements OwsParameterValue {
             this.values.addAll(values);
         }
     }
-    
+
     public boolean isSetValues() {
-    	return CollectionHelper.isNotEmpty(getValues());
+        return CollectionHelper.isNotEmpty(getValues());
     }
 
 }
