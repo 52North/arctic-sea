85bcd5c35244803c97c41aa87706bbc126588028
Daniel Nüst <d.nuest@52north.org>
2015-09-08 13:02:23 +0200

    add name and version strings for fes

--- src/main/java/org/n52/iceland/ogc/filter/FilterConstants.java
+++ src/main/java/org/n52/iceland/ogc/filter/FilterConstants.java
@@ -53,6 +53,12 @@ public interface FilterConstants {
     SchemaLocation FES_110_SCHEMA_LOCATION = new SchemaLocation(NS_FES_110, SCHEMA_LOCATION_URL_FES_110);
 
     String FILTER_LANGUAGE_FES_FILTER = OGCConstants.QUERY_LANGUAGE_PREFIX + "OGC-FES:Filter";
+    
+    String SHORT_NAME = "Filter";
+    
+    String VERSION_STRING_FES_110 = "1.1.0";
+    
+    String VERSION_STRING_FES_2 = "2.0";
 
     /*
      * element names
