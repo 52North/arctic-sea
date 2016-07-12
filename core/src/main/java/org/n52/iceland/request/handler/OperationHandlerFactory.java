3fe771c4dd009ef3d9a9e7eb07cd77309f9d865e
Christian Autermann <c.autermann@52north.org>
2016-07-12 14:27:21 +0200

    new org.n52.iceland.request.handler package

--- core/src/main/java/org/n52/iceland/request/handler/OperationHandlerFactory.java
+++ core/src/main/java/org/n52/iceland/request/handler/OperationHandlerFactory.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.n52.iceland.ds;
+package org.n52.iceland.request.handler;
 
 import org.n52.iceland.component.ComponentFactory;
 
