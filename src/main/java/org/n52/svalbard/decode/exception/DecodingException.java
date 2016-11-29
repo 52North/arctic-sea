da6f5aac440febafbfc946f4efeac911c8387470
Christian Autermann <c.autermann@52north.org>
2016-11-29 11:16:44 +0100

    added more classes

---
+++ src/main/java/org/n52/svalbard/decode/exception/DecodingException.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2016 52°North Initiative for Geospatial Open Source
+ * Software GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.n52.svalbard.decode.exception;
+
+import org.n52.janmayen.exception.LocationHintException;
+
+/**
+ *
+ * @author Matthes Rieke <m.rieke@52north.org>
+ */
+public class DecodingException extends LocationHintException {
+    private static final long serialVersionUID = 1864784275478310843L;
+
+    public DecodingException(String message, Object... args) {
+        super(message, args);
+    }
+
+    public DecodingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DecodingException(Throwable cause) {
+        super(cause);
+    }
+
+    public DecodingException(String location, String message, Object... args) {
+        super(location, message, args);
+    }
+
+    public DecodingException(Enum<?> location, String message, Object... args) {
+        super(location, message, args);
+    }
+
+    public DecodingException(Throwable cause, Enum<?> location, String message, Object... args) {
+        super(cause, location, message, args);
+    }
+
+    public DecodingException(Throwable cause, String location, String message, Object... args) {
+        super(cause, location, message, args);
+    }
+
+    public DecodingException(Throwable cause, String location) {
+        super(cause, location);
+    }
+
+    public DecodingException(Throwable cause, Enum<?> location) {
+        super(cause, location);
+    }
+
+}
---
+++ src/main/java/org/n52/svalbard/encode/exception/EncodingException.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2016 52°North Initiative for Geospatial Open Source
+ * Software GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.n52.svalbard.encode.exception;
+
+import org.n52.janmayen.exception.LocationHintException;
+
+/**
+ *
+ * @author Matthes Rieke <m.rieke@52north.org>
+ */
+public class EncodingException extends LocationHintException {
+    private static final long serialVersionUID = -7730668591653900392L;
+
+    public EncodingException(String message, Object... args) {
+        super(message, args);
+    }
+
+    public EncodingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EncodingException(Throwable cause) {
+        super(cause);
+    }
+
+    public EncodingException(String location, String message, Object... args) {
+        super(location, message, args);
+    }
+
+    public EncodingException(Enum<?> location, String message, Object... args) {
+        super(location, message, args);
+    }
+
+    public EncodingException(Throwable cause, Enum<?> location, String message, Object... args) {
+        super(cause, location, message, args);
+    }
+
+    public EncodingException(Throwable cause, String location, String message, Object... args) {
+        super(cause, location, message, args);
+    }
+
+    public EncodingException(Throwable cause, String location) {
+        super(cause, location);
+    }
+
+    public EncodingException(Throwable cause, Enum<?> location) {
+        super(cause, location);
+    }
+
+}
