ada15e09fbd60cee807c6148e4d41af5eaa9919d
Christian Autermann <c.autermann@52north.org>
2016-12-01 16:17:47 +0100

    license headers & some package adjustments

--- src/test/java/org/n52/shetland/ogc/gml/TimeTest.java
+++ src/test/java/org/n52/shetland/ogc/gml/TimeTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.n52.iceland.ogc.gml;
+package org.n52.shetland.ogc.gml;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.core.Is.is;
