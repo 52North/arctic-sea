420162c1cbe847492313e9e1f0892a5b51c857a6
Christian Autermann <c.autermann@52north.org>
2017-01-03 12:23:07 +0100

    updated license headers

--- src/main/java/org/n52/shetland/ogc/sos/FeatureType.java
+++ src/main/java/org/n52/shetland/ogc/sos/FeatureType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 52°North Initiative for Geospatial Open Source
+ * Copyright 2016-2017 52°North Initiative for Geospatial Open Source
  * Software GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
