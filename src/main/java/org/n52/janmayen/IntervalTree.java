a41ca80f0906a1105a853e10877687b03c3e9008
CarstenHollmann <c.hollmann@52north.org>
2017-07-20 14:09:25 +0200

    Add license header and fix findbugs

--- src/main/java/org/n52/janmayen/IntervalTree.java
+++ src/main/java/org/n52/janmayen/IntervalTree.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016-2017 52°North Initiative for Geospatial Open Source
+ * Software GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.n52.janmayen;
 
 import static java.util.stream.Collectors.toSet;
@@ -378,6 +394,16 @@ public class IntervalTree<K, V> implements IntervalMap<K, V> {
                 comp = comparator.compare(getUpper(), getUpper());
             }
             return comp;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return Objects.equals(this, obj);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getLower(), 43, getUpper());
         }
 
         /**
