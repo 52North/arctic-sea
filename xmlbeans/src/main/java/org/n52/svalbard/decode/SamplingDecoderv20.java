8bb95584fc0eaf8b921c3bef95878827e5042e56
Christian Autermann <c.autermann@52north.org>
2017-09-19 11:56:01 +0200

    support multi geometries

--- xmlbeans/src/main/java/org/n52/svalbard/decode/SamplingDecoderv20.java
+++ xmlbeans/src/main/java/org/n52/svalbard/decode/SamplingDecoderv20.java
@@ -21,8 +21,17 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
+import net.opengis.gml.x32.FeaturePropertyType;
+import net.opengis.gml.x32.ReferenceType;
+import net.opengis.samplingSpatial.x20.SFSpatialSamplingFeatureDocument;
+import net.opengis.samplingSpatial.x20.SFSpatialSamplingFeatureType;
+import net.opengis.samplingSpatial.x20.ShapeType;
+
 import org.apache.xmlbeans.XmlException;
 import org.apache.xmlbeans.XmlObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.n52.shetland.ogc.OGCConstants;
 import org.n52.shetland.ogc.SupportedType;
 import org.n52.shetland.ogc.gml.AbstractFeature;
@@ -39,8 +48,6 @@ import org.n52.svalbard.decode.exception.DecodingException;
 import org.n52.svalbard.decode.exception.UnsupportedDecoderXmlInputException;
 import org.n52.svalbard.util.CodingHelper;
 import org.n52.svalbard.util.XmlHelper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableSet;
@@ -48,14 +55,11 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.MultiLineString;
+import com.vividsolutions.jts.geom.MultiPoint;
+import com.vividsolutions.jts.geom.MultiPolygon;
 import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.geom.Polygon;
-
-import net.opengis.gml.x32.FeaturePropertyType;
-import net.opengis.gml.x32.ReferenceType;
-import net.opengis.samplingSpatial.x20.SFSpatialSamplingFeatureDocument;
-import net.opengis.samplingSpatial.x20.SFSpatialSamplingFeatureType;
-import net.opengis.samplingSpatial.x20.ShapeType;
 
 /**
  * @since 1.0.0
@@ -241,14 +245,14 @@ public class SamplingDecoderv20
     }
 
     private String getFeatTypeForGeometry(final Geometry geometry) {
-        if (geometry instanceof Point) {
+        if (geometry instanceof Point || geometry instanceof MultiPoint) {
             return SfConstants.SAMPLING_FEAT_TYPE_SF_SAMPLING_POINT;
-        } else if (geometry instanceof LineString) {
+        } else if (geometry instanceof LineString || geometry instanceof MultiLineString) {
             return SfConstants.SAMPLING_FEAT_TYPE_SF_SAMPLING_CURVE;
-        } else if (geometry instanceof Polygon) {
+        } else if (geometry instanceof Polygon || geometry instanceof MultiPolygon) {
             return SfConstants.SAMPLING_FEAT_TYPE_SF_SAMPLING_SURFACE;
         }
-        return OGCConstants.UNKNOWN;
+        return SfConstants.SAMPLING_FEAT_TYPE_SF_SAMPLING_FEATURE;
     }
 
 }
