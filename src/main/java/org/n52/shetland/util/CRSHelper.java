711243cb56ed334175aa528b7b37ea28f3c66e98
Christian Autermann <c.autermann@52north.org>
2017-09-19 14:47:21 +0200

    formatting

--- src/main/java/org/n52/shetland/util/CRSHelper.java
+++ src/main/java/org/n52/shetland/util/CRSHelper.java
@@ -29,13 +29,11 @@ public final class CRSHelper {
     }
 
     public static String asHttpPrefix(String prefix) {
-        return (!prefix.endsWith("/") && !prefix.isEmpty() && prefix
-                .startsWith("http")) ? prefix + "/" : prefix;
+        return (!prefix.endsWith("/") && !prefix.isEmpty() && prefix.startsWith("http")) ? prefix + "/" : prefix;
     }
 
     public static String asUrnPrefix(String prefix) {
-        return (!prefix.endsWith(":") && !prefix.isEmpty() && prefix
-                .startsWith("urn")) ? prefix + ":" : prefix;
+        return (!prefix.endsWith(":") && !prefix.isEmpty() && prefix.startsWith("urn")) ? prefix + ":" : prefix;
     }
 
     /**
