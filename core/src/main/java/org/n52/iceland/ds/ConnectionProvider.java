abb9a49ebb9d2240ce48f10505a8df86e5f777a7
Carsten Hollmann <c.hollmann@52north.org>
2018-01-02 13:01:09 +0100

    update license header

--- core/src/main/java/org/n52/iceland/ds/ConnectionProvider.java
+++ core/src/main/java/org/n52/iceland/ds/ConnectionProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2017 52°North Initiative for Geospatial Open Source
+ * Copyright 2015-2018 52°North Initiative for Geospatial Open Source
  * Software GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -39,10 +39,10 @@ public interface ConnectionProvider {
      * @param connection the connection
      */
     void returnConnection(Object connection);
-    
+
     /**
      * Get the max connections number
-     * 
+     *
      * @return The max connection number
      */
     int getMaxConnections();
