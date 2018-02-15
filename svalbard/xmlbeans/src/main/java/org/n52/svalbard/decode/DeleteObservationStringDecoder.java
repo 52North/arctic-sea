f1121b3013d2f91b7ce7a2f21bcf846878192f5b
Carsten Hollmann <c.hollmann@52north.org>
2018-02-15 16:58:16 +0100

    Add missing request decoder

---
+++ svalbard/xmlbeans/src/main/java/org/n52/svalbard/decode/DeleteObservationStringDecoder.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2015-2018 52°North Initiative for Geospatial Open Source
+ * Software GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.n52.svalbard.decode;
+
+import java.util.Collections;
+import java.util.Set;
+
+import org.n52.shetland.ogc.sos.Sos2Constants;
+import org.n52.shetland.ogc.sos.delobs.DeleteObservationConstants;
+import org.n52.svalbard.util.CodingHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Joiner;
+
+/**
+ * @author <a href="mailto:e.h.juerrens@52north.org">Eike Hinderk
+ *         J&uuml;rrens</a>
+ *
+ * @since 1.0.0
+ */
+public class DeleteObservationStringDecoder
+        extends
+        AbstractStringRequestDecoder {
+
+    private static final Set<DecoderKey> DECODER_KEYS = CodingHelper.xmlStringDecoderKeysForOperationAndMediaType(
+            Sos2Constants.SOS, Sos2Constants.SERVICEVERSION, DeleteObservationConstants.Operations.DeleteObservation);
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DeleteObservationStringDecoder.class);
+
+    public DeleteObservationStringDecoder() {
+        LOGGER.info("Decoder for the following keys initialized successfully: {}!",
+                Joiner.on(", ").join(DECODER_KEYS));
+    }
+
+    @Override
+    public Set<DecoderKey> getKeys() {
+        return Collections.unmodifiableSet(DECODER_KEYS);
+    }
+
+}
