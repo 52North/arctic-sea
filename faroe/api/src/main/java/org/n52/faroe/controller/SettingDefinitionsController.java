56f1a36aa87092c8d91dd3b70888b5f357f0a2f9
T Vignesh <tvvignes@amazon.com>
2019-06-24 18:31:57 +0530

    Fix checkstyle issues

--- faroe/api/src/main/java/org/n52/faroe/controller/SettingDefinitionsController.java
+++ faroe/api/src/main/java/org/n52/faroe/controller/SettingDefinitionsController.java
@@ -28,7 +28,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
 import java.util.HashMap;
--- faroe/api/src/main/java/org/n52/faroe/controller/SettingsController.java
+++ faroe/api/src/main/java/org/n52/faroe/controller/SettingsController.java
@@ -29,13 +29,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.inject.Inject;
 import java.util.Map;
 import java.util.Optional;
 
 import org.n52.faroe.Constants;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * This controller is responsible for creation of endpoints to modify settings more easily.
