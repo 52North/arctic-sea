faa5de0fdc24ac175d51eeffb2e77dd25019a94a
Carsten Hollmann <c.hollmann@52north.org>
2019-07-11 10:45:26 +0200

    change decoding of numeric settings which can be of type BigDecimal and double to check for number

--- faroe/json/src/main/java/org/n52/faroe/json/JsonSettingsDao.java
+++ faroe/json/src/main/java/org/n52/faroe/json/JsonSettingsDao.java
@@ -125,7 +125,7 @@ public class JsonSettingsDao extends AbstractJsonDao implements SettingsDao {
                 }
                 return node.intValue();
             case NUMERIC:
-                if (!(node.isDouble() || node.isBigDecimal())) {
+                if (!node.isNumber()) {
                     numberDecodeError(type, node);
                 }
                 return node.doubleValue();
