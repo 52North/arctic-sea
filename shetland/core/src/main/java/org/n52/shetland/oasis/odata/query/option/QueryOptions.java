dd6fb6f885b4ec42da1cf0a2c5b16b3d591c629f
speckij <speckij@gmail.com>
2020-02-17 14:37:14 +0100

    refactor QueryOptions to be parsed by antlr4 parser

--- shetland/core/src/main/java/org/n52/shetland/oasis/odata/query/option/QueryOptions.java
+++ shetland/core/src/main/java/org/n52/shetland/oasis/odata/query/option/QueryOptions.java
@@ -14,20 +14,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.n52.shetland.oasis.odata.query.option;
 
 import org.n52.shetland.filter.CountFilter;
+import org.n52.shetland.filter.ExpandFilter;
+import org.n52.shetland.filter.FilterFilter;
+import org.n52.shetland.filter.OrderByFilter;
+import org.n52.shetland.filter.SelectFilter;
 import org.n52.shetland.filter.SkipTopFilter;
-import org.n52.shetland.oasis.odata.ODataConstants;
-import org.n52.shetland.ogc.filter.Filter;
 import org.n52.shetland.ogc.filter.FilterClause;
 import org.n52.shetland.ogc.filter.FilterConstants;
 import org.n52.shetland.ogc.filter.FilterConstants.SkipTopOperator;
 
-import java.util.LinkedHashSet;
-import java.util.Optional;
 import java.util.Set;
-import java.util.function.Predicate;
 
 /**
  * Class to hold Query Parameters
@@ -41,19 +41,38 @@ public class QueryOptions {
 
     private String baseURL;
 
-    private Set<FilterClause> queryOptions = new LinkedHashSet<>();
-
-    public QueryOptions(String baseURL) {
-        this.baseURL = baseURL;
-        this.queryOptions.add(new SkipTopFilter(SkipTopOperator.Top, DEFAULT_TOP));
-    }
+    private CountFilter countOption;
+    private OrderByFilter orderByOption;
+    private SelectFilter selectOption;
+    private ExpandFilter expandOption;
+    private SkipTopFilter skipOption;
+    private SkipTopFilter topOption;
+    private FilterFilter filterOption;
 
     public QueryOptions(String baseURL, Set<FilterClause> queryOptions) {
         this.baseURL = baseURL;
         if (queryOptions != null) {
-            this.queryOptions.addAll(queryOptions);
+            queryOptions.forEach((input) -> {
+                if (input instanceof CountOption) {
+                    countOption = (CountFilter) input;
+                } else if (input instanceof OrderByOption) {
+                    orderByOption = (OrderByFilter) input;
+                } else if (input instanceof SelectOption) {
+                    selectOption = (SelectFilter) input;
+                } else if (input instanceof ExpandOption) {
+                    expandOption = (ExpandFilter) input;
+                } else if (input instanceof SkipTopFilter &&
+                        ((SkipTopFilter) input).getOperator().equals(FilterConstants.SkipTopOperator.Skip)) {
+                    skipOption = (SkipTopFilter) input;
+                } else if (input instanceof SkipTopFilter &&
+                        ((SkipTopFilter) input).getOperator().equals(FilterConstants.SkipTopOperator.Top)) {
+                    topOption = (SkipTopFilter) input;
+                } else if (input instanceof FilterOption) {
+                    filterOption = (FilterFilter) input;
+                }
+            });
             if (!hasTopOption()) {
-                this.queryOptions.add(new SkipTopFilter(SkipTopOperator.Top, DEFAULT_TOP));
+                this.topOption = new SkipTopFilter(SkipTopOperator.Top, DEFAULT_TOP);
             }
         }
     }
@@ -63,111 +82,66 @@ public class QueryOptions {
     }
 
     public boolean hasCountOption() {
-        return has(ODataConstants.QueryOptions.COUNT);
+        return countOption != null;
     }
 
     public CountFilter getCountOption() {
-        return hasCountOption() ? (CountFilter) find(ODataConstants.QueryOptions.COUNT).get() : null;
+        return hasCountOption() ? countOption : null;
     }
 
     public boolean hasTopOption() {
-        return has(ODataConstants.QueryOptions.TOP);
+        return topOption != null;
     }
 
     public SkipTopFilter getTopOption() {
-        return hasTopOption() ? (SkipTopFilter) find(ODataConstants.QueryOptions.TOP).get() : null;
+        return hasTopOption() ? topOption : null;
     }
 
     public boolean hasSkipOption() {
-        return has(ODataConstants.QueryOptions.SKIP);
+        return skipOption != null;
     }
 
     public SkipTopFilter getSkipOption() {
-        return hasSkipOption() ? (SkipTopFilter) find(ODataConstants.QueryOptions.SKIP).get() : null;
+        return hasSkipOption() ? skipOption : null;
     }
 
     public boolean hasOrderByOption() {
-        return has(ODataConstants.QueryOptions.ORDERBY);
+        return orderByOption != null;
     }
 
     public OrderByOption getOrderByOption() {
-        return hasOrderByOption() ? (OrderByOption) find(ODataConstants.QueryOptions.ORDERBY).get() : null;
+        return hasOrderByOption() ? orderByOption : null;
     }
 
     public boolean hasSelectOption() {
-        return has(ODataConstants.QueryOptions.SELECT);
+        return selectOption != null;
     }
 
     public SelectOption getSelectOption() {
-        return hasSelectOption() ? (SelectOption) find(ODataConstants.QueryOptions.SELECT).get() : null;
+        return hasSelectOption() ? selectOption : null;
     }
 
     public boolean hasExpandOption() {
-        return has(ODataConstants.QueryOptions.EXPAND);
+        return expandOption != null;
     }
 
     public ExpandOption getExpandOption() {
-        return hasExpandOption() ? (ExpandOption) find(ODataConstants.QueryOptions.EXPAND).get() : null;
+        return hasExpandOption() ? expandOption : null;
     }
 
     public boolean hasFilterOption() {
-        return has(ODataConstants.QueryOptions.FILTER);
+        return filterOption != null;
     }
 
-    public Filter<?> getFilterOption() {
-        return hasFilterOption() ? (Filter<?>) find(ODataConstants.QueryOptions.FILTER).get() : null;
+    public FilterOption getFilterOption() {
+        return hasFilterOption() ? filterOption : null;
     }
 
-    private Optional<FilterClause> find(String name) {
-        return find(new QueryOptionPredicate(name));
-    }
-
-    private Optional<FilterClause> find(Predicate<FilterClause> predicate) {
-        if (hasQueryOptions()) {
-            return queryOptions.stream().filter(predicate).findFirst();
-        }
-        return Optional.empty();
-    }
-
-    private boolean has(String name) {
-        return find(name).isPresent();
-    }
-
-    private boolean hasQueryOptions() {
-        return queryOptions != null && !queryOptions.isEmpty();
-    }
-
+    /*
+    // BaseURI is not always set -> we need to compare each option individually
     @Override
     public boolean equals(Object o) {
         return (o instanceof QueryOptions) && ((QueryOptions) o).getBaseURI().equals(this.getBaseURI());
     }
-
-    private static final class QueryOptionPredicate implements Predicate<FilterClause> {
-
-        private final String name;
-
-        QueryOptionPredicate(String name) {
-            this.name = name;
-        }
-
-        @Override
-        public boolean test(FilterClause input) {
-            if (ODataConstants.QueryOptions.COUNT.equalsIgnoreCase(name) && input instanceof CountOption) {
-                return true;
-            } else if (ODataConstants.QueryOptions.ORDERBY.equalsIgnoreCase(name) && input instanceof OrderByOption) {
-                return true;
-            } else if (ODataConstants.QueryOptions.SELECT.equalsIgnoreCase(name) && input instanceof SelectOption) {
-                return true;
-            } else if (ODataConstants.QueryOptions.EXPAND.equalsIgnoreCase(name) && input instanceof ExpandOption) {
-                return true;
-            } else if (ODataConstants.QueryOptions.SKIP.equalsIgnoreCase(name) && input instanceof SkipTopFilter) {
-                return ((SkipTopFilter) input).getOperator().equals(FilterConstants.SkipTopOperator.Skip);
-            } else if (ODataConstants.QueryOptions.TOP.equalsIgnoreCase(name) && input instanceof SkipTopFilter) {
-                return ((SkipTopFilter) input).getOperator().equals(FilterConstants.SkipTopOperator.Top);
-            } else if (ODataConstants.QueryOptions.FILTER.equalsIgnoreCase(name) && input instanceof Filter) {
-                return true;
-            }
-            return false;
-        }
-    }
+    */
 }
---
+++ svalbard/odata/src/main/java/org/n52/svalbard/odata/QueryOptionsFactory.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2015-2020 52°North Initiative for Geospatial Open Source
+ * Software GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.n52.svalbard.odata;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+import org.n52.shetland.oasis.odata.query.option.QueryOptions;
+import org.n52.svalbard.odata.grammar.ODataQueryParserLexer;
+import org.n52.svalbard.odata.grammar.ODataQueryParserParser;
+
+/**
+ * @author <a href="mailto:j.speckamp@52north.org">Jan Speckamp</a>
+ */
+public class QueryOptionsFactory {
+
+    private ODataQueryParserLexer lexer;
+    private ODataQueryParserParser parser;
+    private ODataQueryVisitor visitor = new ODataQueryVisitor();
+
+    //TODO: make nicer
+    public QueryOptions createQueryOptions(String query) {
+        lexer = new ODataQueryParserLexer(new ANTLRInputStream(query));
+        parser = new ODataQueryParserParser(new CommonTokenStream(lexer));
+        parser.addErrorListener(new BaseErrorListener() {
+            @Override
+            public void syntaxError(Recognizer<?, ?> recognizer,
+                                    Object offendingSymbol,
+                                    int line,
+                                    int charPositionInLine,
+                                    String msg,
+                                    RecognitionException e) {
+                throw new IllegalStateException("failed to parse due to " + msg + " with " +
+                                                        "offending token: " + lexer.getVocabulary()
+                                                                                   .getDisplayName(e.getOffendingToken()
+                                                                                                    .getType()), e);
+            }
+        });
+        return parser.queryOptions().<QueryOptions>accept(visitor);
+    }
+}
