a88e7acc6f65bf79a1dd9b440bb8c23c1af8a9d6
Carsten Hollmann <c.hollmann@52north.org>
2020-04-24 15:36:50 +0200

    Add keys, ... for InspireXmlDecoder to avoid NPE when Capabilities contains INSPIRE extension

--- svalbard/xmlbeans/src/main/java/org/n52/svalbard/decode/InspireXmlDecoder.java
+++ svalbard/xmlbeans/src/main/java/org/n52/svalbard/decode/InspireXmlDecoder.java
@@ -16,12 +16,54 @@
  */
 package org.n52.svalbard.decode;
 
+import java.util.Collections;
+import java.util.Set;
+
+import org.apache.xmlbeans.XmlObject;
+import org.apache.xmlbeans.impl.values.XmlAnyTypeImpl;
+import org.n52.shetland.inspire.InspireConstants;
+import org.n52.shetland.inspire.InspireSupportedCRS;
+import org.n52.shetland.inspire.InspireSupportedLanguages;
+import org.n52.shetland.inspire.dls.InspireExtendedCapabilities;
+import org.n52.shetland.ogc.ows.extension.CapabilitiesExtension;
+import org.n52.svalbard.decode.exception.DecodingException;
+import org.n52.svalbard.util.CodingHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.Sets;
+
 /**
  * XML decoder class for the INSPIRE schema
+ *
  * @author <a href="mailto:c.hollmann@52north.org">Carsten Hollmann</a>
  * @since 1.0.0
  *
  */
-public class InspireXmlDecoder {
+public class InspireXmlDecoder extends AbstractXmlDecoder<XmlObject, Object> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(InspireXmlDecoder.class);
+
+    private static final Set<DecoderKey> DECODER_KEYS = Sets.union(
+            CodingHelper.decoderKeysForElements(InspireConstants.NS_INSPIRE_DLS, XmlAnyTypeImpl.class,
+                    InspireExtendedCapabilities.class),
+            CodingHelper.decoderKeysForElements(InspireConstants.NS_INSPIRE_COMMON, XmlAnyTypeImpl.class,
+                    InspireSupportedLanguages.class, InspireSupportedCRS.class));
+
+    public InspireXmlDecoder() {
+        LOGGER.debug("Decoder for the following keys initialized successfully: {}!",
+                Joiner.on(", ").join(DECODER_KEYS));
+    }
+    @Override
+    public Set<DecoderKey> getKeys() {
+        return Collections.unmodifiableSet(DECODER_KEYS);
+    }
+
+    @Override
+    public Object decode(XmlObject objectToDecode) throws DecodingException {
+        // TODO implement parsing
+        return new CapabilitiesExtension<>();
+    }
 
 }
