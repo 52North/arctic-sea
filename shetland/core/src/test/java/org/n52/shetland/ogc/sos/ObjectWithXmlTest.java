bab5252e484221e5752932cf64511a91bf9ff615
Carsten Hollmann <c.hollmann@52north.org>
2020-06-10 15:23:46 +0200

    fix test and build

--- shetland/core/src/test/java/org/n52/shetland/ogc/sos/ObjectWithXmlTest.java
+++ shetland/core/src/test/java/org/n52/shetland/ogc/sos/ObjectWithXmlTest.java
@@ -1,6 +1,22 @@
+/*
+ * Copyright 2015-2020 52°North Initiative for Geospatial Open Source
+ * Software GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.n52.shetland.ogc.sos;
 
-import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -104,6 +120,5 @@ public class ObjectWithXmlTest {
         public TestObject(Integer object, String xml) {
             super(object, xml);
         }
-
     }
 }
