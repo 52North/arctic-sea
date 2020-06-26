842637e3c4aafb1d37392dced9c218a50df2e98d
Carsten Hollmann <c.hollmann@52north.org>
2020-06-26 08:27:48 +0200

    use xml stream writer

--- svalbard/xmlbeans/src/main/java/org/n52/svalbard/encode/WsdlEncoderv11.java
+++ svalbard/xmlbeans/src/main/java/org/n52/svalbard/encode/WsdlEncoderv11.java
@@ -16,33 +16,38 @@
  */
 package org.n52.svalbard.encode;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
 import java.util.Collections;
 import java.util.Set;
 
+import javax.xml.stream.XMLStreamException;
+
 import org.apache.xmlbeans.XmlObject;
-import org.apache.xmlbeans.impl.xb.substwsdl.DefinitionsDocument;
-import org.apache.xmlbeans.impl.xb.substwsdl.DefinitionsDocument.Definitions;
 import org.n52.janmayen.http.MediaType;
 import org.n52.janmayen.http.MediaTypes;
-import org.n52.shetland.w3c.wsa.WsaConstants;
-import org.n52.shetland.w3c.wsa.WsaHeader;
+import org.n52.shetland.w3c.wsdl.Definitions;
+import org.n52.shetland.w3c.wsdl.WSDLConstants;
 import org.n52.svalbard.encode.exception.EncodingException;
 import org.n52.svalbard.util.CodingHelper;
+import org.n52.svalbard.write.Wsdlv11XmlStreamWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Joiner;
 
-public class WsdlEncoderv11 extends AbstractXmlEncoder<XmlObject, Object> {
+public class WsdlEncoderv11 extends AbstractXmlEncoder<XmlObject, Definitions>
+        implements StreamingEncoder<XmlObject, Definitions> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(WsdlEncoderv11.class);
 
-    private static final Set<EncoderKey> ENCODER_KEYS = Collections.EMPTY_SET;
-//            CodingHelper.encoderKeysForElements(WSDLCo.NS_WSA, WsaHeader.class);
+    private static final Set<EncoderKey> ENCODER_KEYS =
+            CodingHelper.encoderKeysForElements(WSDLConstants.NS_WSDL, Definitions.class);
 
     public WsdlEncoderv11() {
-        LOGGER.debug("Encoder for the following keys initialized successfully: {}!",
-                Joiner.on(", ").join(ENCODER_KEYS));
+        LOGGER.debug("Encoder for the following keys initialized successfully: {}!", Joiner.on(", ")
+                .join(ENCODER_KEYS));
     }
 
     @Override
@@ -56,12 +61,31 @@ public class WsdlEncoderv11 extends AbstractXmlEncoder<XmlObject, Object> {
     }
 
     @Override
-    public XmlObject encode(Object objectToEncode, EncodingContext additionalValues) throws EncodingException {
-        DefinitionsDocument doc = DefinitionsDocument.Factory.newInstance(getXmlOptions());
-        Definitions def = doc.addNewDefinitions();
-        XmlObject addNewService = def.addNewService();
-        // TODO Auto-generated method stub
-        return null;
+    public XmlObject encode(Definitions element, EncodingContext ctx) throws EncodingException {
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            encode(element, baos, ctx);
+            ByteArrayInputStream in = new ByteArrayInputStream(baos.toByteArray());
+            return XmlObject.Factory.parse(in);
+        } catch (Exception e) {
+            throw new EncodingException(e);
+        }
+    }
+
+    @Override
+    public void encode(Definitions element, OutputStream outputStream, EncodingContext ctx)
+            throws EncodingException {
+        try {
+            if (!ctx.has(EncoderFlags.ENCODER_REPOSITORY)) {
+                new Wsdlv11XmlStreamWriter(ctx.with(EncoderFlags.ENCODER_REPOSITORY, getEncoderRepository())
+                        .with(StreamingEncoderFlags.EMBEDDED, false), outputStream, element).write();
+            } else {
+                new Wsdlv11XmlStreamWriter(ctx.with(StreamingEncoderFlags.EMBEDDED, false), outputStream, element)
+                        .write();
+            }
+        } catch (XMLStreamException xmlse) {
+            throw new EncodingException("Error while writing element to stream!", xmlse);
+        }
+
     }
 
 }
