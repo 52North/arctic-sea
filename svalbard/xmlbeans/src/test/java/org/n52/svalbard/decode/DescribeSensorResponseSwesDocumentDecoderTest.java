0d461791a819b39023cb2e6502163e23b83892d2
Carsten Hollmann <c.hollmann@52north.org>
2021-01-19 15:33:56 +0100

    add decoding of sensor descriptions

--- svalbard/xmlbeans/src/test/java/org/n52/svalbard/decode/DescribeSensorResponseSwesDocumentDecoderTest.java
+++ svalbard/xmlbeans/src/test/java/org/n52/svalbard/decode/DescribeSensorResponseSwesDocumentDecoderTest.java
@@ -6,8 +6,10 @@ import java.util.Arrays;
 import org.apache.xmlbeans.XmlException;
 import org.apache.xmlbeans.XmlObject;
 import org.apache.xmlbeans.XmlOptions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.n52.shetland.ogc.sensorML.SensorMLConstants;
 import org.n52.shetland.ogc.sos.response.DescribeSensorResponse;
 import org.n52.svalbard.decode.exception.DecodingException;
 import org.n52.svalbard.decode.exception.DescribeSensorResponseSwesDocumentDecoder;
@@ -16,27 +18,36 @@ import org.n52.svalbard.util.CodingHelper;
 
 public class DescribeSensorResponseSwesDocumentDecoderTest {
 
-    private DescribeSensorResponseSwesDocumentDecoder decoder;
     private DecoderRepository decoderRepository;
 
     @BeforeEach
     public void setup() {
         decoderRepository = new DecoderRepository();
 
-        decoder = new DescribeSensorResponseSwesDocumentDecoder();
+        DescribeSensorResponseSwesDocumentDecoder decoder = new DescribeSensorResponseSwesDocumentDecoder();
         decoder.setDecoderRepository(decoderRepository);
         decoder.setXmlOptions(XmlOptions::new);
+        
+        SensorMLDecoderV101 sensorMLDecoderV101 = new SensorMLDecoderV101();
+        sensorMLDecoderV101.setDecoderRepository(decoderRepository);
+        sensorMLDecoderV101.setXmlOptions(XmlOptions::new);
 
-        decoderRepository.setDecoders(Arrays.asList(decoder));
+        SweCommonDecoderV101 sweCommonDecoderV101 = new SweCommonDecoderV101();
+        sweCommonDecoderV101.setDecoderRepository(decoderRepository);
+        sweCommonDecoderV101.setXmlOptions(XmlOptions::new);
+
+        decoderRepository.setDecoders(Arrays.asList(decoder, sensorMLDecoderV101, sweCommonDecoderV101));
         decoderRepository.init();
     }
 
     @Test
-    public void should_parse_DWithin_Filter() throws DecodingException, XmlException, IOException {
+    public void should_parse() throws DecodingException, XmlException, IOException {
         XmlObject xml = decode("/DescribeSensorResponseSML101.xml");
         DecoderKey decoderKey = CodingHelper.getDecoderKey(xml);
         Decoder<DescribeSensorResponse, XmlObject> decoder = decoderRepository.getDecoder(decoderKey);
-        DescribeSensorResponse doc = decoder.decode(xml);
+        DescribeSensorResponse response = decoder.decode(xml);
+        Assertions.assertEquals(SensorMLConstants.NS_SML, response.getOutputFormat());
+        Assertions.assertEquals(1, response.getProcedureDescriptions().size());
     }
 
     private XmlObject decode(String fileName) throws DecodingException, XmlException, IOException {
