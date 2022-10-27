2d65e5353e32a23c11593f9155cfcf5929ce8064
CarstenHollmann <c.hollmann@52north.org>
2022-10-27 15:33:54 +0200

    Add where operations and tests

--- shetland/arcgis/src/main/java/org/n52/shetland/arcgis/service/feature/FeatureServiceConstants.java
+++ shetland/arcgis/src/main/java/org/n52/shetland/arcgis/service/feature/FeatureServiceConstants.java
@@ -15,6 +15,10 @@
  */
 package org.n52.shetland.arcgis.service.feature;
 
+import java.util.Arrays;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
 public interface FeatureServiceConstants {
 
     interface Parameter {
@@ -137,6 +141,163 @@ public interface FeatureServiceConstants {
         String GEO_JSON = "geojson";
         String JSON = "json";
         String PROTOBUF = "pbf";
+    }
+
+    interface WhereOperators {
+
+        String GREATER_THAN = ">=";
+        String LESS_THAN = "<=";
+        String GREATER = ">";
+        String LESS = "<";
+        String EQUAL = "=";
+        String NOT_EQUAL = "<>";
+        String LIKE = "LIKE";
+        String AND = "AND";
+        String OR = "OR";
+        String IS = "IS";
+        String IS_NOT = "IS_NOT";
+        String IN = "IN";
+        String NOT_IN = "NOT_IN";
+        String BETWEEEN = "BETWEEN";
+
+        default String create(String operator, String field, String value) {
+            return create(operator, field, value, "'");
+        }
+
+        default String create(String operator, String field, String value, String quote) {
+            return new StringBuffer().append(field).append(" ").append(operator).append(" ").append(quote)
+                    .append(value).append(quote).toString();
+        }
+
+        default String create(String operator, String field, Number value) {
+            return new StringBuffer().append(field).append(" ").append(operator).append(" ").append(value).toString();
+        }
+
+        default String createIn(String... values) {
+            return new StringBuffer().append("(")
+                    .append(String.join(",",
+                            Arrays.stream(values).map(v -> new StringBuffer("'").append(v.toString()).append("'"))
+                                    .collect(Collectors.toCollection(TreeSet::new))))
+                    .append(")").toString();
+        }
+
+        default String createIn(Number... values) {
+            return new StringBuffer().append("(").append(String.join(",",
+                    Arrays.stream(values).map(v -> v.toString()).collect(Collectors.toCollection(TreeSet::new))))
+                    .append(")").toString();
+        }
+
+        default String greaterThan(String field, String value) {
+            return create(GREATER_THAN, field, value);
+        }
+
+        default String greaterThan(String field, Number value) {
+            return create(GREATER_THAN, field, value);
+        }
+
+        default String lessThan(String field, String value) {
+            return create(LESS_THAN, field, value);
+        }
+
+        default String lessThan(String field, Number value) {
+            return create(LESS_THAN, field, value);
+        }
+
+        default String greater(String field, String value) {
+            return create(GREATER, field, value);
+        }
+
+        default String greater(String field, Number value) {
+            return create(GREATER, field, value);
+        }
+
+        default String less(String field, String value) {
+            return create(LESS, field, value);
+        }
+
+        default String less(String field, Number value) {
+            return create(LESS, field, value);
+        }
+
+        default String equal(String field, String value) {
+            return create(EQUAL, field, value);
+        }
+
+        default String equal(String field, Number value) {
+            return create(EQUAL, field, value);
+        }
+
+        default String notEqual(String field, String value) {
+            return create(NOT_EQUAL, field, value);
+        }
+
+        default String notEqual(String field, Number value) {
+            return create(NOT_EQUAL, field, value);
+        }
+
+        default String like(String field, String value) {
+            return create(LIKE, field, value);
+        }
+
+        default String like(String field, Number value) {
+            return create(LIKE, field, value);
+        }
+
+        default String and(String field, String value) {
+            return create(AND, field, value, "");
+        }
+
+        default String and(String field, Number value) {
+            return create(AND, field, value);
+        }
+
+        default String or(String field, String value) {
+            return create(OR, field, value, "");
+        }
+
+        default String or(String field, Number value) {
+            return create(OR, field, value);
+        }
+
+        default String is(String field, String value) {
+            return create(IS, field, value);
+        }
+
+        default String is(String field, Number value) {
+            return create(IS, field, value);
+        }
+
+        default String isNot(String field, String value) {
+            return create(IS_NOT, field, value);
+        }
+
+        default String isNot(String field, Number value) {
+            return create(IS_NOT, field, value);
+        }
+
+        default String in(String field, String... values) {
+            return create(IN, field, createIn(values), "");
+        }
+
+        default String in(String field, Number... values) {
+            return create(IN, field, createIn(values), "");
+        }
+
+        default String notIn(String field, String... values) {
+            return create(NOT_IN, field, createIn(values), "");
+        }
+
+        default String notIn(String field, Number... values) {
+            return create(NOT_IN, field, createIn(values), "");
+        }
+
+        default String between(String field, String value1, String value2) {
+            return create(AND, create(BETWEEEN, field, value1), value2);
+        }
+
+        default String between(String field, Number value1, Number value2) {
+            return and(create(BETWEEEN, field, value1), value2);
+        }
     }
 
 }
