b5a0d3e78f0614c54d464c69ff25f84036d3a1e8
Sujit Jaunjal <jaunjalsujit@gmail.com>
2023-04-03 12:26:02 +0530

    Updated Springrest and other files

--- faroe/rest/src/main/java/org/n52/faroeREST/springrest/SpringrestApplication.java
+++ faroe/rest/src/main/java/org/n52/faroeREST/springrest/SpringrestApplication.java
@@ -40,8 +40,8 @@ public class SpringrestApplication {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
-        	
-        	
+
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
             	 registry.addMapping("/**")
